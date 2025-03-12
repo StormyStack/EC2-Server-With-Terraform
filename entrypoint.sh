de2de5e5997953351d3a7ebc3ac709c9e9d64003
sudipto <sudiptorocks@proton.me>
2025-03-12 21:29:21 +0600

    Move shell script in another file

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo docker run --name myapp_nginx -d -p 8080:80 nginx
+EOF
--- main.tf
+++ main.tf
@@ -100,27 +100,7 @@ resource "aws_instance" "myapp-server" {
   availability_zone           = var.availability_zones
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
-  user_data                   = <<-EOF
-#!/bin/bash
-
-sudo apt-get update -y
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo systemctl enable --now docker
-
-sudo docker run --name myapp_nginx -d -p 8080:80 nginx
-EOF
+  user_data                   =file("entrypoint.sh")
   tags = {
     Name = "${var.env_prefix}-server"
   }
