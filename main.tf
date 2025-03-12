203c0ac693b77b36a4897ce93d4f96eb50b8ce02
sudipto <sudiptorocks@proton.me>
2025-03-12 18:41:25 +0600

    Run a Latest Ubuntu EC2

--- main.tf
+++ main.tf
@@ -69,3 +69,38 @@ resource "aws_default_security_group" "default-sg" {
     Name = "${var.env_prefix}-default-sg"
   }
 }
+
+data "aws_ami" "LatestUbuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.LatestUbuntu.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "myapp-key-pair"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.LatestUbuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.myapp_subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.availability_zones
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -21,3 +21,13 @@ variable "my-ip" {
   description = "The IP address to allow SSH access."
   type        = string
 }
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance."
+  type        = string
+}
+
+variable "public_key_location" {
+  description = "The location of the public key."
+  type        = string
+}
